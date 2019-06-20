17876f28811b44e69e97759621f9cf122d6567dc
Roopesh V S <txtmeroopesh@gmail.com>
2019-06-20 10:04:43 +0530

    modified:   Fetcher/settings.py

--- Fetcher/settings.py
+++ Fetcher/settings.py
@@ -89,7 +89,7 @@ WSGI_APPLICATION = 'Fetcher.wsgi.application'
     }
 }'''
 
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+DATABASES = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 
 # Password validation
