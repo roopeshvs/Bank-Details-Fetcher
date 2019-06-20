63f0fb925b36c67224efc2a0bcda0197a8b6a1e4
Roopesh V S <txtmeroopesh@gmail.com>
2019-06-20 10:37:46 +0530

    modified:   Fetcher/settings.py

--- Fetcher/settings.py
+++ Fetcher/settings.py
@@ -96,7 +96,7 @@ DATABASES = {
     }
 }
 
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
