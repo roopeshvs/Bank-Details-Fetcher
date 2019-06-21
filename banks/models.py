7d62a411a7b118c799702efb60f8b5d4ab8cc587
Roopesh V S <txtmeroopesh@gmail.com>
2019-06-21 11:59:24 +0530

    modified:   banks/models.py 	modified:   curl.txt

--- banks/models.py
+++ banks/models.py
@@ -7,7 +7,7 @@
 # Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
 
-'''class AuthGroup(models.Model):
+class AuthGroup(models.Model):
     name = models.CharField(unique=True, max_length=80)
 
     class Meta:
@@ -114,7 +114,6 @@ class DjangoSession(models.Model):
     class Meta:
         managed = False
         db_table = 'django_session'
-'''
 
 class Banks(models.Model):
     name = models.CharField(max_length=49, blank=True, null=True)
