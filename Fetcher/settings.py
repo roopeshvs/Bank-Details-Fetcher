db0a573f96184790707674a262ca5c5428165333
Roopesh V S <txtmeroopesh@gmail.com>
2019-06-21 12:04:18 +0530

    modified:   Fetcher/settings.py

--- Fetcher/settings.py
+++ Fetcher/settings.py
@@ -125,7 +125,7 @@ STATIC_URL = '/static/'
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
- #   'DEFAULT_PERMISSION_CLASS': ('rest_framework.permissions.IsAuthenticated',),
+    'DEFAULT_PERMISSION_CLASS': ('rest_framework.permissions.IsAuthenticated',),
     'DEFAULT_AUTHENTICATION_CLASSES': [
     'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
