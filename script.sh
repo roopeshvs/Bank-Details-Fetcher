3d9a4e3e21cf2bea1275a8f198d5c2acd0728d86
Roopesh V S <txtmeroopesh@gmail.com>
2019-06-24 19:40:29 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -3,7 +3,7 @@ TOKEN=$(curl -s -X POST -H 'Accept: application/json' -H 'Content-Type: applicat
 
 curl http://secure-woodland-74914.herokuapp.com/banks/?limit=10&offset=20 && echo "1 - Bank List"
 curl -H 'Accept: application/json' -H "Authorization: Bearer ${TOKEN}" http://secure-woodland-74914.herokuapp.com/branchlist/?limit=15&offset=100 && echo "2 - Branches List"
-curl -H 'Accept: application/json' -H "Authorization: Bearer ${TOKEN}" http://secure-woodland-74914.herokuapp.com/branches/MUMBAI/ALLAHABAD%20BANK?limit=5&offset=10 && echo "3 - Endpoint with City name and Bank name - Example 1" 
+curl -H 'Accept: application/json' -H "Authorization: Bearer ${TOKEN}" http://secure-woodland-74914.herokuapp.com/branches/MUMBAI/ALLAHABAD%20BANK && echo "3 - Endpoint with City name and Bank name - Example 1" 
 curl -H 'Accept: application/json' -H "Authorization: Bearer ${TOKEN}" http://secure-woodland-74914.herokuapp.com/branches/COIMBATORE/ALLAHABAD%20BANK && echo "4 - Endpoint with City name and Bank name - Example 2" 
 curl -H 'Accept: application/json' -H "Authorization: Bearer ${TOKEN}" http://secure-woodland-74914.herokuapp.com/ifsc/ABHY0065009 && echo "5 - Endpoint with IFSC code" 
 $SHELL
