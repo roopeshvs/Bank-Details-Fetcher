17a31645edf41750c2ab1d6d00db2deb89e9137e
Roopesh V S <txtmeroopesh@gmail.com>
2019-06-24 18:49:22 +0530

    modified:   Fetcher/settings.py 	modified:   script.sh

--- Fetcher/settings.py
+++ Fetcher/settings.py
@@ -124,7 +124,6 @@ STATIC_URL = '/static/'
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 10,
     'DEFAULT_PERMISSION_CLASS': ('rest_framework.permissions.IsAuthenticated',),
     'DEFAULT_AUTHENTICATION_CLASSES': [
     'rest_framework_simplejwt.authentication.JWTAuthentication',
--- script.sh
+++ script.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 TOKEN=$(curl -s -X POST -H 'Accept: application/json' -H 'Content-Type: application/json' --data '{"username":"roopesh","password":"roopesh"}' https://secure-woodland-74914.herokuapp.com/api/token/ | ./jq-win64.exe -r '.access')
 
-curl http://secure-woodland-74914.herokuapp.com/banks/ && echo "1 - Bank List" &&
+curl http://secure-woodland-74914.herokuapp.com/banks/?limit=20&offset=2 && echo "1 - Bank List"
 curl -H 'Accept: application/json' -H "Authorization: Bearer ${TOKEN}" http://secure-woodland-74914.herokuapp.com/branchlist/ && echo "2 - Branches List"
-curl -H 'Accept: application/json' -H "Authorization: Bearer ${TOKEN}" http://secure-woodland-74914.herokuapp.com/branches/MUMBAI/ALLAHABAD%20BANK && echo "3 - Endpoint with City name and Bank name - Example 1" &&
-curl -H 'Accept: application/json' -H "Authorization: Bearer ${TOKEN}" http://secure-woodland-74914.herokuapp.com/branches/COIMBATORE/ALLAHABAD%20BANK && echo "4 - Endpoint with City name and Bank name - Example 2" &&
-curl -H 'Accept: application/json' -H "Authorization: Bearer ${TOKEN}" http://secure-woodland-74914.herokuapp.com/ifsc/ABHY0065009 && echo "5 - Endpoint with IFSC code - Example 1" &&
-curl -H 'Accept: application/json' -H "Authorization: Bearer ${TOKEN}" http://secure-woodland-74914.herokuapp.com/ifsc/ABHY0065011 && echo "6 - Endpoint with IFSC code - Example 2"
+curl -H 'Accept: application/json' -H "Authorization: Bearer ${TOKEN}" http://secure-woodland-74914.herokuapp.com/branches/MUMBAI/ALLAHABAD%20BANK?limit=5&offset=10 && echo "3 - Endpoint with City name and Bank name - Example 1" 
+curl -H 'Accept: application/json' -H "Authorization: Bearer ${TOKEN}" http://secure-woodland-74914.herokuapp.com/branches/COIMBATORE/ALLAHABAD%20BANK && echo "4 - Endpoint with City name and Bank name - Example 2" 
+curl -H 'Accept: application/json' -H "Authorization: Bearer ${TOKEN}" http://secure-woodland-74914.herokuapp.com/ifsc/ABHY0065009 && echo "5 - Endpoint with IFSC code" 
 $SHELL
